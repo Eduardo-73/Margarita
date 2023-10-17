39aecfb651cca9c294a6f1df343de7fc837f12ce
ecorara446 <ecorara446@g.educaand.es>
2023-10-17 12:43:27 +0200

    seguir en casas

--- src/main/java/margarita/Margarita.java
+++ src/main/java/margarita/Margarita.java
@@ -3,6 +3,7 @@
  */
 package margarita;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,66 +16,45 @@ public class Margarita {
     public static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-       
-        meQuiereNoMeQuiere();
-
-    }
-
-    private static int dado() {
-        Random random = new Random();
-        int num = random.nextInt(1,7);
-        while (num == 3){
-            System.out.println("Ha salido el 3 el número de la mala suerte,"
-                    + "vuelves a tirar");
-            num = random.nextInt(1,7);
-        }
-        return num;
-    }
-
-    private static int margEvaluar() {
-        int num = 0;
+        String jugar = "";
         do {
-            try {
-                System.out.println("Elige el número de margariatas (1-10):");
-                num = teclado.nextInt();
-                System.out.println("Número de margarita " + num);
-            } catch (Exception e) {
-                System.out.println("Tienes que elegir un número que se "
-                        + "encuentre entre los parametros ");
+            System.out.println("Quieres repetir?");
+            jugar = teclado.nextLine();
+        } while (jugar.equalsIgnoreCase("Si"));
+    }
+
+    private static int preguntaMargaritas() {
+        int numMargaritas = 0;
+        try {
+            do {
+                System.out.println("¿Cuántas margaritas tienes (entre 1 y 10)?");
+                numMargaritas = teclado.nextInt();
+            } while (!(numMargaritas >= 1 && numMargaritas <= 10));
+        } catch (InputMismatchException ime) {
+            System.out.println("Error!!!");
+        }
+        System.out.println("Número margaritas " + numMargaritas);
+        return numMargaritas;
+    }
+
+    private static int tiradaDado() {
+        Random ran = new Random();
+        int num;
+        do {
+            num = ran.nextInt(1, 6);
+            if (num % 2 == 0) {
+                System.out.println("La tirada fue " + num + " me quiere ");
+            } else {
+                System.out.println("La tirada fue " + num + "no me quiere");
             }
-        } while (num > 0 && num < 11);
+        } while (num == 3);
         return num;
     }
-
-    private static boolean meQuiereNoMeQuiere() {
-        int hoja = 0;
-        hoja++;
-        int dado = dado();
-        System.out.println("¿Cuántas hojas tiene la margarita N" + hoja + "?");
-        int numhojas = teclado.nextInt();
-        if (dado % 2 == 0) {
-            System.out.println("La tirada fue " + dado + " y empieza por "
-                    + "'me quiere'");
-        } else {
-            System.out.println("La tirada fue " + dado + " y empieza por "
-                    + "'no me quiere'");
-        }
-        return true;
-                
-    }
-
-    private static int contarMargaritas() {
-
-        int numMarg = 0;
-        System.out.println("Quieres iniciar el programa 'Si/No' ");
-        String iniciar = teclado.nextLine();
-
-        if (iniciar.equalsIgnoreCase("si")) {
-            margEvaluar();
-        } else {
-            System.out.println("Tu te lo pierdes!! ");
-        }
-        return numMarg;
+    
+    private static int numeroHojas(){
+        int contador = 1;
+        System.out.println("Cuaántas hojas tiene la margarita Nº?" + contador);
+        int numeroHojas = teclado.nextInt();
+        
     }
 }
